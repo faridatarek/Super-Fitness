9b3bc11841e327d6359741d1290352b04e16ef8b
Abdalla El-daly <abdalla90eldaly90@gmail.com>
2025-02-17 04:51:18 +0200

    finish splash screen and onboarding_screen

--- lib/features/intro/onboarding_screen/data/on_boarding_data.dart
+++ lib/features/intro/onboarding_screen/data/on_boarding_data.dart
@@ -1,19 +1,21 @@
+import '../../../../utils/assets_manager.dart';
+
 class OnBoardingData {
  static final List<Map<String, String>> onboardingData = [
     {
-      "image": "assets/images/O15_31.png",
+      "image": ImageAssets.onBoarding3,
       "title": "The Price Of Excellence\nIs Discipline",
       "description":
       "Success requires commitment. Stay consistent, push your limits, and see the results you deserve."
     },
     {
-      "image": "assets/images/O15_30.png",
+      "image": ImageAssets.onBoarding2,
       "title": "Fitness Has Never Been So Much Fun",
       "description":
       "Transform your workouts into exciting challenges. Enjoy the journey, not just the destination!"
     },
     {
-      "image": "assets/images/O15_29.png",
+      "image": ImageAssets.onBoarding1,
       "title": "NO MORE EXCUSES\nDo It Now",
       "description":
       "Your future self will thank you. Stop waiting, take action, and achieve greatness today!"
