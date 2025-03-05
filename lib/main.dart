bc8dd4ac25adeb685e1081e8d78ca0312dba04ef
faridatarek <faridaelamrety732@gmail.com>
2025-03-05 23:28:25 +0200

    edit in main for provider to use the same instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
